50c563db630f63df79be8bec1209f6181b33fee7
Two <Two@192.168.107.23>
2018-03-12 22:14:09 +0500

    Добавление тестов с разными значениями

---
+++ src/Tests/Test_1.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import main.rentCalc;
+
+class Test_1 {
+
+	@Test
+	void test() 
+	{
+		rentCalc t0;
+		
+		// gaz, heat, hot, cold, electr, V_gaz, V_heat, V_hot, V_cold, V_electr
+		t0 = new rentCalc(20,10,10,10,10,-1,2,3,4,5);
+		assertEquals(10, t0.total_houseGas);
+		assertEquals(20, t0.total_houseHeating);
+		assertEquals(30, t0.total_hotWater);
+		assertEquals(40, t0.total_coldWater);
+		assertEquals(50, t0.total_houseElectricity);
+
+	}
+
+}
+
