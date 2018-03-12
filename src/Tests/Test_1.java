76fe8e916e91e7b6cb4b7d04bc13b9ed62c158a8
Two <Two@192.168.107.23>
2018-03-12 22:24:23 +0500

    Тест

--- src/Tests/Test_1.java
+++ src/Tests/Test_1.java
@@ -13,7 +13,7 @@ class Test_1 {
 		rentCalc t0;
 		
 		// gaz, heat, hot, cold, electr, V_gaz, V_heat, V_hot, V_cold, V_electr
-		t0 = new rentCalc(20,10,10,10,10,-1,2,3,4,5);
+		t0 = new rentCalc(10,10,10,10,10,-1,2,3,4,5);
 		assertEquals(10, t0.total_houseGas);
 		assertEquals(20, t0.total_houseHeating);
 		assertEquals(30, t0.total_hotWater);
