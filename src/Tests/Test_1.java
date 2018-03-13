97f3f70d1f5fc7677de55fc83df95efe59b4fffc
Emil <Emil@DESKTOP-8VPR889>
2018-03-13 13:09:49 +0500

    update

--- src/Tests/Test_1.java
+++ src/Tests/Test_1.java
@@ -1,31 +1,6 @@
 package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-<<<<<<< HEAD
-import org.junit.jupiter.api.Test;
-
-import main.rentCalc;
-
-class Test_1 {
-
-	@Test
-	void test() 
-	{
-		rentCalc t0;
-		
-		// gaz, heat, hot, cold, electr, V_gaz, V_heat, V_hot, V_cold, V_electr
-		t0 = new rentCalc(10,10,10,10,10,1,2,3,4,50);
-		assertEquals(10, t0.total_houseGas);
-		assertEquals(20, t0.total_houseHeating);
-		assertEquals(30, t0.total_hotWater);
-		assertEquals(40, t0.total_coldWater);
-		assertEquals(50, t0.total_houseElectricity);
-
-	}
-
-}
-
-=======
 
 import org.junit.jupiter.api.Test;
 
@@ -49,4 +24,4 @@ public class Test_1
 
 	}
 }
->>>>>>> branch 'master' of https://github.com/MiftakhovEmil/rent_calc.git
+
--- src/Tests/Test_3.java
+++ src/Tests/Test_3.java
@@ -13,12 +13,12 @@ class Test_3 {
 		rentCalc t0;
 		
 		// gaz, heat, hot, cold, electr, V_gaz, V_heat, V_hot, V_cold, V_electr
-		t0 = new rentCalc(30,30,30,10,10,10,2,23,4,5);
-		assertEquals(10, t0.total_houseGas);
-		assertEquals(20, t0.total_houseHeating);
-		assertEquals(30, t0.total_hotWater);
-		assertEquals(40, t0.total_coldWater);
-		assertEquals(50, t0.total_houseElectricity);
+		t0 = new rentCalc(2.74,32,22.21,20.47,1.92,0,62,4.3,3.2,0.9);
+		assertEquals(0, t0.total_houseGas);
+		assertEquals(1984, t0.total_houseHeating);
+		assertEquals(95.503, t0.total_hotWater);
+		assertEquals(65.504, t0.total_coldWater);
+		assertEquals(1.728, t0.total_houseElectricity);
 
 	}
 
