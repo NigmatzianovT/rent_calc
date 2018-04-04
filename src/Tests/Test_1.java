89cf848f765f5df902f301386232769fb90783a7
One1 <One1@DESKTOP-AGNDB3D>
2018-04-05 00:13:04 +0500

    UPDATANEW

--- src/Tests/Test_1.java
+++ src/Tests/Test_1.java
@@ -18,13 +18,13 @@ public class Test_1
 		// ןונגûו 5 - עאנטפû, געמנûו 5 - מבתולû
 		// gaz, heat, hot, cold, electr, V_gaz, V_heat, V_hot, V_cold, V_electr
 		
-		t0 = new rentCalc(10,10,10,10,10,1,2,3,4,510);
+		t0 = new rentCalc(15,16,17,18,19,5,4,3,2,1);
 		
-		assertEquals(10, t0.total_houseGas);
-		assertEquals(20, t0.total_houseHeating);
-		assertEquals(30, t0.total_hotWater);
-		assertEquals(40, t0.total_coldWater);
-		assertEquals(50, t0.total_houseElectricity);//yf
+		assertEquals(75, t0.total_houseGas);
+		assertEquals(64, t0.total_houseHeating);
+		assertEquals(51, t0.total_hotWater);
+		assertEquals(36, t0.total_coldWater);
+		assertEquals(20, t0.total_houseElectricity);
 
 	}
 }
