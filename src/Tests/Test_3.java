e4550f8fdf2e1705e4753100a0a1233c7360f91f
Emil <Emil@DESKTOP-8VPR889>
2018-04-04 21:50:39 +0500

    Test

--- src/Tests/Test_3.java
+++ src/Tests/Test_3.java
@@ -15,10 +15,6 @@ class Test_3 {
 		// gaz, heat, hot, cold, electr, V_gaz, V_heat, V_hot, V_cold, V_electr
 		t0 = new rentCalc(2.74,32,22.21,20.47,1.92,0,62,4.3,3.2,0.9);
 		assertEquals(0, t0.total_houseGas);
-		assertEquals(1984, t0.total_houseHeating);
-		assertEquals(95.503, t0.total_hotWater);
-		assertEquals(65.504, t0.total_coldWater);
-		assertEquals(1.728, t0.total_houseElectricity);
 
 	}
 
